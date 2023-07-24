da818a1ea548610478ab22bc981fcc85312bfb52
NiloLP <112017951+NiloLP@users.noreply.github.com>
2023-07-24 17:54:29 +0000

    lastest

--- src/main/java/com/example/demo/InstrumentoRepository.java
+++ src/main/java/com/example/demo/InstrumentoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "intrumentos", path = "instrumentos")
+@RepositoryRestResource(collectionResourceRel = "instrumentos", path = "instrumentos")
 public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento, Integer>,CrudRepository<Instrumento, Integer>{
     List<Instrumento> findByNombre(@Param("nombre") String nombre);
 }
