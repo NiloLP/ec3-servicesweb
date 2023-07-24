6c478983c16a3aa106647ed569dd2ce5bac95d5a
NiloLP <112017951+NiloLP@users.noreply.github.com>
2023-07-24 17:20:21 +0000

    ec3 now

---
+++ src/main/java/com/example/demo/Categoria.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+    private String nombre;
+
+    @OneToMany(targetEntity = Instrumento.class, mappedBy = "categoria")
+    @OrderBy("nombre ASC")
+    private Set<Instrumento> instrumento = new HashSet<Instrumento>();
+
+    public Integer getId() {
+        return Id;
+    }
+    public void setId(Integer id) {
+        Id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+   
+}
